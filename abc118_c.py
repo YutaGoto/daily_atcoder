bd8d53c724365361a5766d6709a61f56012a699d
YutaGoto <you.goto.510@gmail.com>
2020-08-26 20:51:48 +0900

    add 2020-08-26

---
+++ abc118_c.py
@@ -0,0 +1,10 @@
+import math
+from functools import reduce
+
+def gcd_list(numbers):
+    return reduce(math.gcd, numbers)
+
+n = input()
+l = [ int(x) for x in input().split() ]
+
+print(gcd_list(l))
