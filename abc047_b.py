079a64093eb39578d7c87a5276826232ad8de319
YutaGoto <you.goto.510@gmail.com>
2020-09-02 22:51:49 +0900

    add 2020-09-02

---
+++ abc047_b.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+w, h, n = map(int, input().split())
+area = np.zeros((w, h))
+
+for i in range(n):
+    x, y, a = map(int, input().split())
+    if a==1:
+        area[:x, :] = 1
+    elif a==2:
+        area[x:, :] = 1
+    elif a==3:
+        area[:, :y] = 1
+    else:
+        area[:, y:] = 1
+
+print((w*h) - int(area.sum()))
