f970bb98c27c9c36125296cf9491a0e96c6effad
YutaGoto <you.goto.510@gmail.com>
2020-10-01 23:07:36 +0900

    add 2020-10-01

---
+++ abc148_c.py
@@ -0,0 +1,17 @@
+def gcd(m, n):
+    if m < n:
+        m, n = n, m
+
+    while True:
+        r = m % n
+        if r == 0:
+            return n
+        else:
+            m, n = n, r
+
+def lcm(m, n):
+    return m*n // gcd(m, n)
+
+a, b = map(int, input().split())
+
+print(lcm(a, b))
