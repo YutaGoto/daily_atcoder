c03d1b587dafab52247fd20dc3018581cc75a517
YutaGoto <you.goto.510@gmail.com>
2021-01-11 21:31:24 +0900

    add 2021-01-11

---
+++ abc004_2.py
@@ -0,0 +1,14 @@
+c1 = [ x for x in input().split() ]
+c2 = [ x for x in input().split() ]
+c3 = [ x for x in input().split() ]
+c4 = [ x for x in input().split() ]
+
+c1.reverse()
+c2.reverse()
+c3.reverse()
+c4.reverse()
+
+print(' '.join(c4))
+print(' '.join(c3))
+print(' '.join(c2))
+print(' '.join(c1))
