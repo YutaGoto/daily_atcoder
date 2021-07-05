1cf43e15af1c844eae16a23cc9ac49c06e936535
YutaGoto <you.goto.510@gmail.com>
2021-07-05 21:38:04 +0900

    add 2021-07-05

---
+++ 2021-07/cf17-final_a.py
@@ -0,0 +1,7 @@
+s = input()
+akiba = ['AKIHABARA','KIHABARA', 'AKIHBARA', 'AKIHABRA', 'AKIHABAR', 'KIHBARA', 'KIHABRA', 'KIHABAR', 'AKIHBRA', 'AKIHBAR', 'AKIHABR', 'KIHBRA', 'KIHBAR', 'KIHABR', 'AKIHBR', 'KIHBR']
+
+if s in akiba:
+    print('YES')
+else:
+    print('NO')
